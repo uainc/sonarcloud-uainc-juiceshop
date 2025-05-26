d1a6ee0e223d603293afc5af36d2eaf40d055678
Andre Kolodochka <59625655+andrekolodochka@users.noreply.github.com>
2025-05-26 10:16:10 +1000

    change to JS

--- playground/ipaddress.cs
+++
@@ -1,6 +0,0 @@
-public IPAddress Case15()
-{
-  var ip = "192.168.12.42";
-  var address = IPAddress.Parse(ip);
-  return address;
-}
---
+++ playground/ipaddress.js
@@ -0,0 +1,5 @@
+function case15() {
+  const ip = "192.168.12.42";
+
+  return ip;
+}
