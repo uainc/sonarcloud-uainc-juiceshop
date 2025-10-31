23446d219bcb2b124bd8116edc8c7d7c792b1be1
Andre Kolodochka <59625655+datanerd@users.noreply.github.com>
2025-10-31 12:14:10 +1100

    Update cwe89.js

--- cwe89.js
+++ cwe89.js
@@ -42,3 +42,19 @@ app.delete("/api/delete", async (req, res) => {
 
   res.json({ status: "ok" });
 });
+
+
+const app = require("express")(),
+      pg = require("pg"),
+      pool = new pg.Pool(config);
+
+app.get("search", function handler(req, res) {
+  // BAD: the category might have SQL special characters in it
+  var query1 =
+    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +
+    req.params.category +
+    "' ORDER BY PRICE";
+  pool.query(query1, [], function(err, results) {
+    // process results
+  });
+});
