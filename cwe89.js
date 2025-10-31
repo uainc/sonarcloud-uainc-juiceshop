98cd40224c738d7494d030bb4f1655ca7e55a453
Andre Kolodochka <59625655+datanerd@users.noreply.github.com>
2025-10-31 12:13:22 +1100

    Update cwe89.js

--- cwe89.js
+++ cwe89.js
@@ -1,3 +1,30 @@
+GITHUB_TOKEN = "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"
+
+if not a == 2:
+    b = not i < 10
+
+def func(a, b, compute):
+    i = a + b  
+    i = compute()
+    return ((i))
+
+for i in range(10):
+    pass
+
+for (i = 0; i < 10; i++) {
+  if (i == 5) {
+    continue;  /* Noncompliant */
+  }
+  alert("i = " + i);
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+sum(1, 2, 3);
+
+
 const mongoose = require("mongoose");
 const Todo = mongoose.model(
   "Todo",
